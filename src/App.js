1065d5c3ff0c4d155c82577ca27c4de5fd92fe51
Eddahkarambu <gikunaeddah@gmail.com>
2021-09-23 16:21:39 +0300

    updated app.js, getting the formula to get time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import React,{useRef,useState} from 'react';
 import './App.css';
 
 function App() {
+  const[timerDays, setTimerDays]=useState('00');
+  const[timerHours, setTimerHours]=useState('00');
+  const[timerMinutes, setTimerMinutes]=useState('00');
+  const[timerSeconds, setTimerSeconds]=useState('00');
+
+
+  let interval=useRef();
+
+  const startTimer =()=>{
+    const countdownDate= new Date('October 10 2021 00:00:00').getTime();
+
+    interval=setInterval(()=>{
+      const now = new Date().getTime();
+      const distance=countdownDate -now;
+
+
+      const days= Math.floor(distance/(1000*60*60*24));
+      const hours= Math.floor((distance%(1000*60*60*24)/(1000*60*60)));
+      const minutes= Math.floor((distance%(1000*60*60*24))/(1000*60));
+      const seconds= Math.floor((distance%(1000*60*60*24))/1000);
+
+    }, 1000);
+
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Timer-container">
+      <div className="Timer">
+        <div>
+      <h1>My 23rd Birthday</h1>
+      <p>Happy Birthday to me in afew :)</p>
+      </div>
+      
+
+      <div>
+        <section>
+          <p>17</p>
+          <p><small>Days</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>17</p>
+          <p><small>Hours</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>17</p>
+          <p><small>Minutes</small></p>
+        </section>
+        <span>:</span>
+
+        <section>
+          <p>17</p>
+          <p><small>Seconds</small></p>
+        </section>
+        
+
+      </div>
+      </div>
+      </div>
     </div>
+
+   
+
+    
   );
 }
 
